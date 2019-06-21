8ba49b6c8c9ed6b650834033e94190b4cb522352
jaro <jarosz.bartlomiej89@gmail.com>
2019-06-21 19:21:37 +0200

    Routing

--- src/layout/FrontPage.js
+++ src/layout/FrontPage.js
@@ -36,7 +36,7 @@ const FrontPage = () => {
 
   return (
     <ThemeProvider theme={theme} >
-      <Router basename={ProcessingInstruction.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL}>
         <GlobalStyle />
         <>
           <Header />
