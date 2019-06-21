25b5ec160cbbfb9a524fd69f12bade84906a7b25
jaro <jarosz.bartlomiej89@gmail.com>
2019-06-21 18:52:04 +0200

    Updates

---
+++ precache-manifest.72064780671cbe78c13af5c5220adb8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee196d1caf13683afe31471f76c1a0d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "03a0db66dae436b365ee",
+    "url": "/portfolio/static/js/2.793be867.chunk.js"
+  },
+  {
+    "revision": "b9d92f5d5e90d3bab06b",
+    "url": "/portfolio/static/js/main.b8364cbe.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9c10bc6c1df9ce54e547ef67b5bfab1f",
+    "url": "/portfolio/static/media/banner_background.9c10bc6c.jpg"
+  },
+  {
+    "revision": "02f5f8f7496da6aecb7ca604652475df",
+    "url": "/portfolio/static/media/butterfly-animation.02f5f8f7.png"
+  },
+  {
+    "revision": "1a7c53e3e22d6825329e090b857fa887",
+    "url": "/portfolio/static/media/chalk_logo.1a7c53e3.png"
+  },
+  {
+    "revision": "76fd08f577b2f02d8f2921ef2749ef56",
+    "url": "/portfolio/static/media/chalk_logo_animation.76fd08f5.png"
+  }
+]);
